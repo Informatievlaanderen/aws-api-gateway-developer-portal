d5e4da2070ec3df54a39caa235bab05437a2a481
David Cumps <david@cumps.be>
2019-10-17 09:13:17 +0200

    fix: dont get api logos

--- dev-portal/src/services/state.js
+++ dev-portal/src/services/state.js
@@ -118,20 +118,20 @@ reactTo(
  * 
  */
 function fetchApiImage(apiList) {
-  apiList.forEach(api => {
-    if (!api.logo) {
-      let specificLogo = `/custom-content/api-logos/${api.id}_${api.stage}.png`
+  // apiList.forEach(api => {
+  //   if (!api.logo) {
+  //     let specificLogo = `/custom-content/api-logos/${api.id}_${api.stage}.png`
   
-      // fetch automatically follows redirects; setting redirect to `manual` prevents this
-      // we need to prevent it so that we can accurately determine if the image exists
-      fetch(specificLogo, { headers: { Accept: "image/png" }, redirect: "manual" }).then(response => {
-        if (response.ok)
-          api.logo = specificLogo
+  //     // fetch automatically follows redirects; setting redirect to `manual` prevents this
+  //     // we need to prevent it so that we can accurately determine if the image exists
+  //     fetch(specificLogo, { headers: { Accept: "image/png" }, redirect: "manual" }).then(response => {
+  //       if (response.ok)
+  //         api.logo = specificLogo
 
-        else api.logo = '/custom-content/api-logos/default.png'
-      })
-    }
-  })
+  //       else api.logo = '/custom-content/api-logos/default.png'
+  //     })
+  //   }
+  // })
 }
 
 /**
